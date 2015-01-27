f13d618de69e16fe97a886e21c0063ffa8f562d5
Leszek Lesner <leszek.lsner@web.de>
2015-01-27 12:35:55 +0100

    Added russian translation

---
+++ translations/harbour-webcat-ru.ts
@@ -0,0 +1,360 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ru" version="2.0">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>A simple webbrowser based on webkit.</source>
+        <translation>Простой веб-браузер на базе WebKit.</translation>
+    </message>
+</context>
+<context>
+    <name>AddBookmark</name>
+    <message>
+        <source>Edit Bookmark</source>
+        <translation>Редактировать закладку</translation>
+    </message>
+    <message>
+        <source>Add Bookmark</source>
+        <translation>Добавить закладку</translation>
+    </message>
+</context>
+<context>
+    <name>AuthenticationDialog</name>
+    <message>
+        <source>Authenticate</source>
+        <translation>Авторизоваться</translation>
+    </message>
+    <message>
+        <source>The server %1 requires authentication. The server says: %2</source>
+        <translation>Сервер %1 требует авторизации. Сервер сообщает: %2</translation>
+    </message>
+    <message>
+        <source>Enter your user name</source>
+        <translation>Введите имя пользователя</translation>
+    </message>
+    <message>
+        <source>User name</source>
+        <translation>Имя пользователя</translation>
+    </message>
+    <message>
+        <source>Enter password</source>
+        <translation>Введите пароль</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Пароль</translation>
+    </message>
+</context>
+<context>
+    <name>ConfirmDialog</name>
+    <message>
+        <source>Accept ?</source>
+        <translation>Принять?</translation>
+    </message>
+</context>
+<context>
+    <name>DownloadManager</name>
+    <message>
+        <source>Add Download</source>
+        <translation>Добавить загрузку</translation>
+    </message>
+    <message>
+        <source>Download Manager</source>
+        <translation>Менеджер загрузки</translation>
+    </message>
+    <message>
+        <source>Enter URL to download</source>
+        <translation>Введите url-адрес для загрузки</translation>
+    </message>
+    <message>
+        <source>Download</source>
+        <translation>Загрузить</translation>
+    </message>
+    <message>
+        <source>Abort</source>
+        <translation>Прервать</translation>
+    </message>
+    <message>
+        <source>Active Downloads: </source>
+        <translation>Активные загрузки:</translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation>Статус</translation>
+    </message>
+    <message>
+        <source>Errors</source>
+        <translation>Ошибки</translation>
+    </message>
+    <message>
+        <source>Download Status</source>
+        <translation>Статус скачивания</translation>
+    </message>
+    <message>
+        <source>Download Errors</source>
+        <translation>Ошибки скачивания</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Enter an url</source>
+        <translation>Ввести url-адрес</translation>
+    </message>
+    <message>
+        <source>Minimize</source>
+        <translation>Уменьшить</translation>
+    </message>
+    <message>
+        <source>New Tab</source>
+        <translation>Новая вкладка</translation>
+    </message>
+    <message>
+        <source>New Window</source>
+        <translation>Новое окно</translation>
+    </message>
+    <message>
+        <source>Reload</source>
+        <translation>Обновить</translation>
+    </message>
+    <message>
+        <source>Lock Orientation</source>
+        <translation>Блокировка поворота экрана</translation>
+    </message>
+    <message>
+        <source>Reader Mode</source>
+        <translation>Режим чтения</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Поиск</translation>
+    </message>
+    <message>
+        <source>Close menu</source>
+        <translation>Закрыть меню</translation>
+    </message>
+    <message>
+        <source>Enter searchterm</source>
+        <translation>Ввести текст для поиска</translation>
+    </message>
+    <message>
+        <source>Open in New Window</source>
+        <translation>Открыть новое окно</translation>
+    </message>
+    <message>
+        <source>Open in New Tab</source>
+        <translation>Открыть новую вкладку</translation>
+    </message>
+    <message>
+        <source>Open in Private New Window</source>
+        <translation>Открыть в новом приватном окне</translation>
+    </message>
+    <message>
+        <source>Copy Link</source>
+        <translation>Копировать ссылку</translation>
+    </message>
+    <message>
+        <source>Save Image</source>
+        <translation>Сохранить изображение</translation>
+    </message>
+    <message>
+        <source>Save Link</source>
+        <translation>Сохранить ссылку</translation>
+    </message>
+</context>
+<context>
+    <name>HistoryPage</name>
+    <message>
+        <source>History</source>
+        <translation>История</translation>
+    </message>
+    <message>
+        <source>Clear History</source>
+        <translation>Очистить историю</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <source>Load Last Session</source>
+        <translation>Загрузить последний сеанс</translation>
+    </message>
+</context>
+<context>
+    <name>SelectUrl</name>
+    <message>
+        <source>Bookmarks</source>
+        <translation>Закладки</translation>
+    </message>
+    <message>
+        <source>Open in new Tab</source>
+        <translation>Открыть в новой вкладке</translation>
+    </message>
+    <message>
+        <source>Open in new Window</source>
+        <translation>Открыть в новом окне</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Редактировать</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Удалить</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>Add Bookmark</source>
+        <translation>Добавить в закладки</translation>
+    </message>
+    <message>
+        <source>History</source>
+        <translation>История</translation>
+    </message>
+    <message>
+        <source>New Tab</source>
+        <translation>Новая вкладка</translation>
+    </message>
+    <message>
+        <source>Close Tab</source>
+        <translation>Закрыть вкладку</translation>
+    </message>
+    <message>
+        <source>Swipe up to open new window</source>
+        <translation>Свайп вверх, что бы открыть новое окно</translation>
+    </message>
+    <message>
+        <source>Swipe up to open private window</source>
+        <translation>Свайп вверх, что бы открыть приватное окно</translation>
+    </message>
+    <message>
+        <source>Swipe up to close tab</source>
+        <translation>Свайп вверх, что бы закрыть вкладку</translation>
+    </message>
+    <message>
+        <source>Loading..</source>
+        <translation>Загрузка...</translation>
+    </message>
+</context>
+<context>
+    <name>SelectionEditPage</name>
+    <message>
+        <source>Copy</source>
+        <translation>Копировать</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Clear cache</source>
+        <translation>Очистить кэш</translation>
+    </message>
+    <message>
+        <source>Clear Cookies and restart</source>
+        <translation>Удалить cookies и перезагрузить</translation>
+    </message>
+    <message>
+        <source>Clear History</source>
+        <translation>Очистить историю</translation>
+    </message>
+    <message>
+        <source>Save Settings</source>
+        <translation>Сохранить настройки</translation>
+    </message>
+    <message>
+        <source>About </source>
+        <translation>О приложении</translation>
+    </message>
+    <message>
+        <source>Download Manager</source>
+        <translation>Менеджер загрузки</translation>
+    </message>
+    <message>
+        <source>Load Defaults</source>
+        <translation>Загрузка значений по умолчанию</translation>
+    </message>
+    <message>
+        <source>Appearance</source>
+        <translation>Внешний вид</translation>
+    </message>
+    <message>
+        <source>Default Font Size</source>
+        <translation>Размер шрифта по умолчанию</translation>
+    </message>
+    <message>
+        <source>Default Fixed Font Size</source>
+        <translation>Фиксированный размер шрифта по умолчанию</translation>
+    </message>
+    <message>
+        <source>Minimum Font Size</source>
+        <translation>Минимальный размер шрифта</translation>
+    </message>
+    <message>
+        <source>Default Orientation</source>
+        <translation>Ориентация по умолчанию</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Основные</translation>
+    </message>
+    <message>
+        <source>Homepage</source>
+        <translation>Домашняя страница</translation>
+    </message>
+    <message>
+        <source>Search Engine:</source>
+        <translation>Поисковый движок:</translation>
+    </message>
+    <message>
+        <source>Custom</source>
+        <translation>Пользовательский </translation>
+    </message>
+    <message>
+        <source>Private Browsing</source>
+        <translation>Приватный серфинг </translation>
+    </message>
+    <message>
+        <source>Add Default Bookmarks</source>
+        <translation>Добавить по умолчанию закладки</translation>
+    </message>
+    <message>
+        <source>User Agent:</source>
+        <translation>Поисковый движок:</translation>
+    </message>
+    <message>
+        <source>Clear Cookies</source>
+        <translation>Очистить cookies-файлы</translation>
+    </message>
+    <message>
+        <source>Start Private Window</source>
+        <translation>Открыть окно в приватном режиме</translation>
+    </message>
+    <message>
+        <source>Load Images</source>
+        <translation>Загружать изображения</translation>
+    </message>
+    <message>
+        <source>Advanced</source>
+        <translation>Расширенный</translation>
+    </message>
+    <message>
+        <source>DNS Prefetch</source>
+        <translation>DNS Prefetch</translation>
+    </message>
+    <message>
+        <source>Offline Web Application Cache</source>
+        <translation>Offline Web Application Cache</translation>
+    </message>
+    <message>
+        <source>Privacy</source>
+        <translation>Конфиденциальность</translation>
+    </message>
+    <message>
+        <source>Clear Cache</source>
+        <translation>Очистить кэш</translation>
+    </message>
+</context>
+</TS>
