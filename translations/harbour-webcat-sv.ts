69a84764bb226cc9be19020859799a42092b1a18
Åke Engelbrektson <eson57@users.noreply.github.com>
2015-06-01 10:22:18 +0200

    Create harbour-webcat-sv.ts
    Swedish translation if wanted. Tested and found correct.

---
+++ translations/harbour-webcat-sv.ts
@@ -0,0 +1,411 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv_SE">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>A simple webbrowser based on webkit.</source>
+        <translation>En enkel webbläsare, baserad på webkit.</translation>
+    </message>
+</context>
+<context>
+    <name>AddBookmark</name>
+    <message>
+        <source>Edit Bookmark</source>
+        <translation>Redigera bokmärke</translation>
+    </message>
+    <message>
+        <source>Add Bookmark</source>
+        <translation>Lägg till bokmärke</translation>
+    </message>
+</context>
+<context>
+    <name>AuthenticationDialog</name>
+    <message>
+        <source>Authenticate</source>
+        <translation>Verifiera</translation>
+    </message>
+    <message>
+        <source>The server %1 requires authentication. The server says: %2</source>
+        <translation>Servern %1 kräver verifiering. Servern säger: %2</translation>
+    </message>
+    <message>
+        <source>Enter your user name</source>
+        <translation>Ange användarnamn</translation>
+    </message>
+    <message>
+        <source>User name</source>
+        <translation>Användarnamn</translation>
+    </message>
+    <message>
+        <source>Enter password</source>
+        <translation>Ange lösenord</translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation>Lösenord</translation>
+    </message>
+</context>
+<context>
+    <name>ConfirmDialog</name>
+    <message>
+        <source>Accept ?</source>
+        <translation>Acceptera?</translation>
+    </message>
+</context>
+<context>
+    <name>DownloadManager</name>
+    <message>
+        <source>Add Download</source>
+        <translation>Lägg till nedladdning</translation>
+    </message>
+    <message>
+        <source>Download Manager</source>
+        <translation>Nedladdningshanterare</translation>
+    </message>
+    <message>
+        <source>Enter URL to download</source>
+        <translation>Ange URL för nedladdning</translation>
+    </message>
+    <message>
+        <source>Download</source>
+        <translation>Ladda ner</translation>
+    </message>
+    <message>
+        <source>Abort</source>
+        <translation>Avbryt</translation>
+    </message>
+    <message>
+        <source>Active Downloads: </source>
+        <translation>Aktiva nedladdningar: </translation>
+    </message>
+    <message>
+        <source>Status</source>
+        <translation>Status</translation>
+    </message>
+    <message>
+        <source>Errors</source>
+        <translation>Fel</translation>
+    </message>
+    <message>
+        <source>Download Status</source>
+        <translation>Nedladdningsstatus</translation>
+    </message>
+    <message>
+        <source>Download Errors</source>
+        <translation>Nedladdningsfel</translation>
+    </message>
+    <message>
+        <source>Opening...</source>
+        <translation>Öppnar...</translation>
+    </message>
+    <message>
+        <source>Show Downloadfolder</source>
+        <translation>Visa nedladdningsmappen</translation>
+    </message>
+    <message>
+        <source>Clear Downloads</source>
+        <translation>Rensa nedladdningar</translation>
+    </message>
+</context>
+<context>
+    <name>FirstPage</name>
+    <message>
+        <source>Enter an url</source>
+        <translation>Ange en URL</translation>
+    </message>
+    <message>
+        <source>Minimize</source>
+        <translation>Minimera</translation>
+    </message>
+    <message>
+        <source>New Tab</source>
+        <translation>Ny flik</translation>
+    </message>
+    <message>
+        <source>New Window</source>
+        <translation>Nytt fönster</translation>
+    </message>
+    <message>
+        <source>Reload</source>
+        <translation>Uppdatera</translation>
+    </message>
+    <message>
+        <source>Lock Orientation</source>
+        <translation>Lås orienteringsläget</translation>
+    </message>
+    <message>
+        <source>Reader Mode</source>
+        <translation>Läsarläge</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Sök</translation>
+    </message>
+    <message>
+        <source>Close menu</source>
+        <translation>Stäng menyn</translation>
+    </message>
+    <message>
+        <source>Opening...</source>
+        <translation>Öppnar...</translation>
+    </message>
+    <message>
+        <source>Enter searchterm</source>
+        <translation>Ange sökterm</translation>
+    </message>
+    <message>
+        <source>Open in New Window</source>
+        <translation>Öppna i nytt fönster</translation>
+    </message>
+    <message>
+        <source>Open in New Tab</source>
+        <translation>Öppna i ny flik</translation>
+    </message>
+    <message>
+        <source>Open in Private New Window</source>
+        <translation>Öppna i nytt privat fönster</translation>
+    </message>
+    <message>
+        <source>Copy Link</source>
+        <translation>Kopiera länk</translation>
+    </message>
+    <message>
+        <source>Save Image</source>
+        <translation>Spara bild</translation>
+    </message>
+    <message>
+        <source>Save Link</source>
+        <translation>Spara länk</translation>
+    </message>
+</context>
+<context>
+    <name>HistoryPage</name>
+    <message>
+        <source>History</source>
+        <translation>Historik</translation>
+    </message>
+    <message>
+        <source>Clear History</source>
+        <translation>Rensa historiken</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <source>Load Last Session</source>
+        <translation>Läs in senaste sessionen</translation>
+    </message>
+</context>
+<context>
+    <name>OpenDialog</name>
+    <message>
+        <source>Opening...</source>
+        <translation>Öppnar...</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <source>Deleting </source>
+        <translation>Tar bort </translation>
+    </message>
+</context>
+<context>
+    <name>SelectUrl</name>
+    <message>
+        <source>Bookmarks</source>
+        <translation>Bokmärken</translation>
+    </message>
+    <message>
+        <source>Open in new Tab</source>
+        <translation>Öppna i ny flik</translation>
+    </message>
+    <message>
+        <source>Open in new Window</source>
+        <translation>Öppna i nytt fönster</translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation>Redigera</translation>
+    </message>
+    <message>
+        <source>Delete</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Add Bookmark</source>
+        <translation>Lägg till bokmärke</translation>
+    </message>
+    <message>
+        <source>History</source>
+        <translation>Historik</translation>
+    </message>
+    <message>
+        <source>New Tab</source>
+        <translation>Ny flik</translation>
+    </message>
+    <message>
+        <source>Close Tab</source>
+        <translation>Stäng flik</translation>
+    </message>
+    <message>
+        <source>Swipe up to open new window</source>
+        <translation>Svep uppåt för att öppna nytt fönster</translation>
+    </message>
+    <message>
+        <source>Swipe up to open private window</source>
+        <translation>Svep uppåt för att öppna privat fönster</translation>
+    </message>
+    <message>
+        <source>Swipe up to close tab</source>
+        <translation>Svep uppåt för att stänga flik</translation>
+    </message>
+    <message>
+        <source>Loading..</source>
+        <translation>Läser in...</translation>
+    </message>
+</context>
+<context>
+    <name>SelectionEditPage</name>
+    <message>
+        <source>Copy</source>
+        <translation>Kopiera</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Clear cache</source>
+        <translation>Rensa cache</translation>
+    </message>
+    <message>
+        <source>Clear History</source>
+        <translation>Rensa historik</translation>
+    </message>
+    <message>
+        <source>Save Settings</source>
+        <translation>Spara inställningar</translation>
+    </message>
+    <message>
+        <source>About </source>
+        <translation>Om </translation>
+    </message>
+    <message>
+        <source>Download Manager</source>
+        <translation>Nedladdningshanterare</translation>
+    </message>
+    <message>
+        <source>Load Defaults</source>
+        <translation>Läs in standard</translation>
+    </message>
+    <message>
+        <source>Appearance</source>
+        <translation>Utseende</translation>
+    </message>
+    <message>
+        <source>Default Font Size</source>
+        <translation>Standardteckenstorlek</translation>
+    </message>
+    <message>
+        <source>Default Fixed Font Size</source>
+        <translation>Standard för fast teckenstorlek</translation>
+    </message>
+    <message>
+        <source>Minimum Font Size</source>
+        <translation>Minsta teckenstorlek</translation>
+    </message>
+    <message>
+        <source>Default Orientation</source>
+        <translation>Standardorientering</translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation>Allmänt</translation>
+    </message>
+    <message>
+        <source>Homepage</source>
+        <translation>Hemsida</translation>
+    </message>
+    <message>
+        <source>Search Engine:</source>
+        <translation>Sökmotor:</translation>
+    </message>
+    <message>
+        <source>Custom</source>
+        <translation>Anpassat</translation>
+    </message>
+    <message>
+        <source>Private Browsing</source>
+        <translation>Privat surfning</translation>
+    </message>
+    <message>
+        <source>Add Default Bookmarks</source>
+        <translation>Lägg till standardbokmärken</translation>
+    </message>
+    <message>
+        <source>User Agent:</source>
+        <translation>User Agent:</translation>
+    </message>
+    <message>
+        <source>Clear Cookies</source>
+        <translation>Rensa cookies</translation>
+    </message>
+    <message>
+        <source>Start Private Window</source>
+        <translation>Öppna privat fönster</translation>
+    </message>
+    <message>
+        <source>Load Images</source>
+        <translation>Läs in bilder</translation>
+    </message>
+    <message>
+        <source>Advanced</source>
+        <translation>Avancerat</translation>
+    </message>
+    <message>
+        <source>DNS Prefetch</source>
+        <translation>DNS-prefetch</translation>
+    </message>
+    <message>
+        <source>Offline Web Application Cache</source>
+        <translation>Nedkopplad programcache</translation>
+    </message>
+    <message>
+        <source>Privacy</source>
+        <translation>Sekretess</translation>
+    </message>
+    <message>
+        <source>Clear Cache</source>
+        <translation>Rensa cache</translation>
+    </message>
+    <message>
+        <source>Clear Bookmarks</source>
+        <translation>Rensa bokmärken</translation>
+    </message>
+    <message>
+        <source>Use external video player</source>
+        <translation>Använd extern videospelare</translation>
+    </message>
+</context>
+<context>
+    <name>VideoPlayer</name>
+    <message>
+        <source>Download</source>
+        <translation>Ladda ner</translation>
+    </message>
+</context>
+<context>
+    <name>ytQualityChooser</name>
+    <message>
+        <source>Opening...</source>
+        <translation>Öppnar...</translation>
+    </message>
+</context>
+</TS>
